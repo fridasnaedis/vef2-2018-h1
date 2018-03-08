1c99abc2b427d73476bcdc42599c36f63b16d172
fridasnaedis <fridasnaedis@gmail.com>
2018-03-08 14:51:38 +0000

    Add createdb.js and update schema

--- schema.sql
+++ schema.sql
@@ -1,11 +1,12 @@
 CREATE TABLE library (
   id serial primary key,
   title character varying(255) NOT NULL,
+  isbn13 char(13) unique,
   author character varying(64),
   description text,
-  isbn13 char(13) unique,
+  category text,
+  isbn10 char(10),
   published date,
   pagecount integer,
-  language text,
-  category text
+  language text
 );
