d5ef4f45eb2b6bf145c0ed1742d8ba4f6c530ece
sakkattack <isakkolbeins@gmail.com>
2018-03-09 01:02:02 +0000

    lagaði load, gerir categories líka núna

--- load.js
+++ load.js
@@ -28,11 +28,19 @@ async function query(q, values = []) {
   }
 
 async function loadToDb(jsonObj) {
+    const categories = [];
     
     for(i=0; i< jsonObj.length; i++ ) {
         const book = jsonObj[i];
+        // Ath hvort category hefur komið annars bæta í categories töflu
+        if (!categories.includes(book.category)) { 
+            const q = 'INSERT INTO categories (category) VALUES($1);';
+            const values = [book.category];
+            await query(q, values);
+            categories.push(book.category);
+        }
+
         const q = 'INSERT INTO library (title, isbn13, author, description, category, isbn10, published, pagecount, language) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);';
-        if (book.pagecount===''){book.pagecount = 0};
         const values = [book.title, book.isbn13, book.author, book.description, book.category, book.isbn10, book.published, book.pagecount, book.language];
         
         await query(q, values);
--- schema.sql
+++ schema.sql
@@ -1,12 +1,38 @@
+CREATE TABLE users (
+	id serial primary key,
+	username varchar(20) unique NOT NULL,
+	password text NOT NULL,
+	name varchar(20) NOT NULL,
+	photoUrl text 
+);
+
+CREATE TABLE categories (
+  id serial primary key,
+  category varchar(30) unique NOT NULL
+);
+
 CREATE TABLE library (
   id serial primary key,
   title character varying(255) NOT NULL,
   isbn13 char(13) unique,
   author character varying(64),
   description text,
-  category text,
+  category varchar(30),
   isbn10 char(10),
   published varchar(10),
-  pagecount integer,
-  language varchar(2)
+  pagecount text,
+  language varchar(2),
+	FOREIGN KEY (category) REFERENCES categories(category)
 );
+
+CREATE TABLE readBooks (
+	id serial,
+	user_id integer NOT NULL,
+	book_id integer NOT NULL, 
+	stars integer NOT NULL,
+	review text,
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (book_id) REFERENCES library(id)
+);
+
+
