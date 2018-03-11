4ea883e66a5b68688e05bd6a1e3a65805e9531c3
Jullira <32617407+Jullira@users.noreply.github.com>
2018-03-11 18:05:56 +0000

    byrja á categories

--- categories.js
+++ categories.js
@@ -1,12 +1,32 @@
 //TODO setja inn rétt imports
 const express = require('express');
 const router = express.Router();
+const { Client } = require('pg');
 
 cosnt xss = require('xss');
 
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/h1';
+
+async function query(q, values = []) {
+    const client = new Client(connectionString);
+    await client.connect();
+
+    let result;
+    try {
+        result = await client.query(q, values);
+    } catch (err) {
+        console.error('err');
+        throw err;
+    } finally {
+        await client.end();
+    }
+    return result;
+}
+
 // Skilar síðu af flokkum
-function getCategories(req, res) {
-    // TODO
+async function getCategories(req, res) {
+    const q = 'SELECT * FROM categories;';
+    const result = await query(q);
 }
 
 // Býr til nýjan flokk og skilar
