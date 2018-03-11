837e4380c8ab55577dbf7ddbfc301649ac3428f2
Jullira <32617407+Jullira@users.noreply.github.com>
2018-03-11 18:33:35 +0000

    útfæra categoriesData

--- categories.js
+++ categories.js
@@ -1,37 +1,19 @@
-//TODO setja inn rétt imports
+
 const express = require('express');
 const router = express.Router();
-const { Client } = require('pg');
+const categoriesData = require('./categoriesData';)
 
-cosnt xss = require('xss');
-
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/h1';
-
-async function query(q, values = []) {
-    const client = new Client(connectionString);
-    await client.connect();
-
-    let result;
-    try {
-        result = await client.query(q, values);
-    } catch (err) {
-        console.error('err');
-        throw err;
-    } finally {
-        await client.end();
-    }
-    return result;
-}
 
 // Skilar síðu af flokkum
 async function getCategories(req, res) {
-    const q = 'SELECT * FROM categories;';
-    const result = await query(q);
+    const categories = getAllCategories();
+    return categories;
 }
 
 // Býr til nýjan flokk og skilar
 function postCategories(req, res) {
-    // TODO
+    const category = PostACategory();
+    return categories;
 }
 
 /* todo útfæra api */
