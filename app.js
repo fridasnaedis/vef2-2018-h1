25847b7c4b6ee1c56ea33b929d85979bd9bfc97d
Hinrik <hinriksnaer@gmail.com>
2018-03-11 16:12:05 +0000

    Added app

---
+++ app.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const users = require('./users');
+const books = require('./books');
+const login = require('./login');
+const categories = require('./categories');
+const myUsers = require('./myUsers');
+
+const app = express();
+
+app.use(express.json());
+app.use('/', api);
+app.use('/users', users);
+app.use('/books', books);
+app.use('/users/me', myUsers);
+app.use('/categories', categories);
+app.use('/register', login);
+app.use('/login', login);
+
+function notFoundHandler(req, res, next) { // eslint-disable-line
+  res.status(404).json({ error: 'Not found' });
+}
+
+function errorHandler(err, req, res, next) { // eslint-disable-line
+  console.error(err);
+
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return res.status(400).json({ error: 'Invalid json' });
+  }
+
+  return res.status(500).json({ error: 'Internal server error' });
+}
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+const {
+  PORT: port = 3000,
+  HOST: host = '127.0.0.1',
+} = process.env;
+
+app.listen(port, () => {
+  console.info(`Server running at http://${host}:${port}/`);
+});
