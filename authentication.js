c6d0a2395cf9dfc910f513768309f5274935c692
Hinrik <hinriksnaer@gmail.com>
2018-03-20 16:54:15 +0000

    added authentication module

---
+++ authentication.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+
+function requireAuthentication(req, res, next) {
+  return passport.authenticate(
+    'jwt',
+    { session: false },
+    (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        const error = info.name === 'TokenExpiredError' ? 'expired token' : 'invalid token';
+        return res.status(401).json({ error });
+      }
+
+      req.user = user;
+      next();
+    },
+  )(req, res, next);
+}
+
+module.exports = {
+  requireAuthentication,
+};
